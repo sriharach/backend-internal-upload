420124a8126a8d30faf6b285a5f00857e39f81a6
folk.sriharach <folk.sriharach@bitkub.com>
2023-08-30 20:25:09 +0700

    up

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const fastify = require('fastify')({ logger: true })
 const helmet = require('@fastify/helmet')
-const { uploadsModule } = require('./upload')
+const { uploadsModule, fileNameFindPublic, removeFileFormPublic } = require('./upload')
 const static = require('@fastify/static')
 const path = require('path')
 const multipart = require('@fastify/multipart')
+const cors = require('@fastify/cors')
 
 fastify.register(
   helmet,
   { contentSecurityPolicy: false }
 )
 
+fastify.register(cors, {
+  origin: true
+})
+
 fastify.register(multipart, {
-  prefix: 'public'
+  prefix: 'public',
+  limits: {
+    fieldSize: 10240,
+}
 })
 
 fastify.register(static, {
@@ -20,8 +28,11 @@ fastify.register(static, {
 })
 
 fastify.post('/internal/upload/:country_id/:ticpid_id', uploadsModule)
+fastify.get('/internal/public/:country_id/:ticpid_id', fileNameFindPublic)
+fastify.delete('/internal/public/:country_id/:ticpid_id/remove', removeFileFormPublic)
 
-fastify.get('/another/path', function (req, reply) {
+fastify.get('/another/path', async function (req, reply) {
+
   reply.sendFile('/image/photo_2566-08-18 14.12.18.jpeg')
 })
 
--- upload.js
+++ upload.js
@@ -28,11 +28,51 @@ exports.uploadsModule = async (request, reply) => {
 
         for await (const part of parts) {
             await pump(part.file, fs.createWriteStream(`${initPath}/${part.filename}`))
-
         }
         reply.status(204)
     } catch (error) {
         reply.send(error)
     }
 
+}
+
+exports.fileNameFindPublic = async (request, reply) => {
+    try {
+        const parts = request.files()
+        const country_id = request.params.country_id
+        const ticpid_id = request.params.ticpid_id
+
+        const founddirs = fs.readdirSync(path.join(__dirname, `/public/${country_id}/${ticpid_id}`))
+
+        let model = {}
+
+        for (let index = 0; index < founddirs.length; index++) {
+            const element = founddirs[index];
+            model.name[index] = `http://127.0.0.1:9000/public/${country_id}/${ticpid_id}/${element}`
+        }
+
+        reply.send(model)
+
+    } catch (error) {
+        reply.send(error)
+    }
+}
+
+exports.removeFileFormPublic = async (request, reply) => {
+    try {
+        const file_name = request.query.file_name
+        const country_id = request.params.country_id
+        const ticpid_id = request.params.ticpid_id
+
+        if (!file_name) {
+            throw new Error('Not found a file name')
+        }
+        const path_dir = path.join(__dirname, `/public/${country_id}/${ticpid_id}/${file_name}`)
+        fs.rmSync(path_dir)
+        reply.status(204)
+
+
+    } catch (error) {
+        reply.send(error)
+    }
 }
